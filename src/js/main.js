4fe9ba22adb70e9a02078afa48b3a7f269a9a4b3
0J3J8A <juanjoaguirre38@gmail.com>
2025-11-12 16:26:44 -0500

    adding driverinitials--team3 activity

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,13 @@
 // src/js/main.js
+import { loadHeaderFooter } from "./utils.mjs";
 import ProductData from './ProductData.mjs';
 import ProductList from './ProductList.mjs';
 
-// Create instance of ProductData
-const productData = new ProductData('tents');
+loadHeaderFooter();
 
-// Get the ul class "product-list"
-const listElement = document.querySelector('.product-list');
+const dataSource = new ProductData("tents");
+
+const element = document.querySelector(".product-list");
 
 // Create instance of ProductList
 const productList = new ProductList("Tents", dataSource, element);
